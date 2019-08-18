a185ce2a4264c374641a21562b5df3ae2b4459ad
Randolph Xia <xia.randolph.227@gmail.com>
2019-08-18 12:37:20 -0400

    Add glad and glew and create a window

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+    std::cout << "resized" << std::endl;
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "opengl-engine", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "glfwCreateWindow failed" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    // window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 1;
+    }
+
+    // main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
